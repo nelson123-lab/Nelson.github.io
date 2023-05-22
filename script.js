8482bc2f0157aae7ec4d80888e9b0a8ec7d68a0b
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-21 23:50:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const offsetTop = target.offsetTop;
+
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
 
